4b274400508cb2cf8e0b7da501723b9165c1d234
Rubbbershredder <amanmuleva@gmail.com>
2025-04-12 05:42:24 +0530

    backend is working

--- server/main.go
+++ server/main.go
@@ -1,1 +1,18 @@
-package server
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+}
